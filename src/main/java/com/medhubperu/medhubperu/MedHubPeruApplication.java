65b658a53abcacefd12444e1d2b72e12e4cf13d0
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-10-31 10:36:27 -0500

    Initial commit

---
+++ src/main/java/com/medhubperu/medhubperu/MedHubPeruApplication.java
@@ -0,0 +1,13 @@
+package com.medhubperu.medhubperu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHubPeruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedHubPeruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medhubperu/medhubperu/MedHubPeruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhubperu.medhubperu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedHubPeruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
